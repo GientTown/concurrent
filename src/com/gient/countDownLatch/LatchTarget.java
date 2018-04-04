7b80ad2ba5b706fe7a3ad59db89af797417a601d
GientTown <492321016@qq.com>
2018-04-04 16:24:32 +0800

    使用CountDownLatch，打印偶数的线程任务

--- src/com/gient/countDownLatch/LatchTarget.java
+++ src/com/gient/countDownLatch/LatchTarget.java
@@ -1,15 +1,32 @@
 package com.gient.countDownLatch;
 
+import java.util.concurrent.CountDownLatch;
+
 public class LatchTarget implements Runnable {
+
+	private CountDownLatch latch;
+
+	public LatchTarget() {
+		super();
+	}
+
+	public LatchTarget(CountDownLatch latch) {
+		super();
+		this.latch = latch;
+	}
 
 	@Override
 	public void run() {
 		synchronized (this) {
-			for (int i = 0; i < 100; i++) {
-				// 打印偶数
-				if (i % 2 == 0) {
-					System.out.println(Thread.currentThread().getName() + "-" + i);
+			try {
+				for (int i = 0; i < 100; i++) {
+					// 打印偶数
+					if (i % 2 == 0) {
+						System.out.println(Thread.currentThread().getName() + "-" + i);
+					}
 				}
+			} finally {
+				latch.countDown();
 			}
 		}
 	}
