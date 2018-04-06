054ec9ad002708309d47adb9abfb83117f18da36
GientTown <492321016@qq.com>
2018-04-06 16:16:17 +0800

    线程任务调度

---
+++ src/com/gient/pool/ScheduleClient.java
@@ -0,0 +1,29 @@
+package com.gient.pool;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduleClient {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+		// 创建线程任务
+		PoolTask task = new PoolTask();
+
+		// 创建线程池
+		ScheduledExecutorService pool = Executors.newScheduledThreadPool(5);
+
+		// 提交任务
+		for (int i = 0; i < 10; i++) {
+			ScheduledFuture<?> future = pool.schedule(task, 2, TimeUnit.SECONDS);
+			future.get();
+		}
+
+		// 关闭线程池
+		pool.shutdown();
+	}
+}
