9dfb337a7c9881c408570e4cde1a2496b67b4c99
GientTown <492321016@qq.com>
2018-04-06 16:17:14 +0800

    future.get()

--- src/com/gient/pool/Client.java
+++ src/com/gient/pool/Client.java
@@ -8,22 +8,22 @@ import java.util.concurrent.Future;
 public class Client {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		
-		//创建线程任务
+
+		// 创建线程任务
 		PoolTask task = new PoolTask();
-		
-		//创建线程池
+
+		// 创建线程池
 		ExecutorService pool = Executors.newFixedThreadPool(5);
-		
-		//提交任务
-		for(int i=0;i<10;i++){
-			Future<?> future = pool.submit(task);
-//			System.out.println(future.get());
+
+		// 提交任务
+		for (int i = 0; i < 10; i++) {
+			pool.submit(task);
+			// System.out.println(future.get());
 		}
-//		System.out.println(future.get());
-		
-		//关闭线程池
+		// System.out.println(future.get());
+
+		// 关闭线程池
 		pool.shutdown();
 	}
-	
+
 }
